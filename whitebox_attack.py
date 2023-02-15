8b665ebe7c10ba1b413b91c91dc876137f4b29d8
csl <734644395@qq.com>
2023-02-15 16:35:47 +0800

    whitebox_attack.py

--- whitebox_attack.py
+++ whitebox_attack.py
@@ -42,7 +42,8 @@ def scores_per_word_from_scores_per_token(input, tokenizer, input_ids, scores_pe
     end_idx = 0
     # TODO: DELETE
     words_from_chars = []
-    print(input,scores_per_id)
+    scores_per_id = scores_per_id[1:-1]
+    #print(input,scores_per_id)
     for inp in input:
         if start_idx >= len(scores_per_id):
             break
